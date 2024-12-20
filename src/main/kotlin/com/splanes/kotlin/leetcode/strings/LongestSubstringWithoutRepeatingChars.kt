eb6dffe659ed8a0a35482a8353149c88489b8b77
splanes <sergio.planes@adevinta.com>
2024-12-20 19:13:16 +0100

    LongestSubstringWithoutRepeatingChars - Code 3 - Medium

--- src/main/kotlin/com/splanes/kotlin/leetcode/strings/LongestSubstringWithoutRepeatingChars.kt
+++ src/main/kotlin/com/splanes/kotlin/leetcode/strings/LongestSubstringWithoutRepeatingChars.kt
@@ -1,5 +1,55 @@
 package com.splanes.kotlin.leetcode.strings
 
+fun main() {
+
+  val longestSubstringWithoutRepeatingChars = LongestSubstringWithoutRepeatingChars()
+
+  var input = "abcabcbb"
+  var expected = 3
+  var actual = longestSubstringWithoutRepeatingChars.find(input)
+
+  println(
+    """
+      Case 1
+      ------
+      Input: $input
+      Expected: $expected
+      Actual: $actual
+    """.trimIndent()
+  )
+  assert(expected == actual)
+
+  input = "bbbbb"
+  expected = 1
+  actual = longestSubstringWithoutRepeatingChars.find(input)
+
+  println(
+    """
+      Case 2
+      ------
+      Input: $input
+      Expected: $expected
+      Actual: $actual
+    """.trimIndent()
+  )
+  assert(expected == actual)
+
+  input = "pwwkew"
+  expected = 3
+  actual = longestSubstringWithoutRepeatingChars.find(input)
+
+  println(
+    """
+      Case 3
+      ------
+      Input: $input
+      Expected: $expected
+      Actual: $actual
+    """.trimIndent()
+  )
+  assert(expected == actual)
+}
+
 /**
  * ## Description
  * ### Code: 3
@@ -15,4 +65,32 @@ package com.splanes.kotlin.leetcode.strings
  * Explanation: The answer is "abc", with the length of 3.
  */
 class LongestSubstringWithoutRepeatingChars {
+
+  fun find(anyString: String): Int {
+
+    val chars = anyString.toCharArray()
+    val founds = mutableListOf<Char>()
+    val aux = mutableListOf<Char>()
+    var array = chars
+
+    for (char in chars) {
+      for (c in array) {
+        if (!aux.contains(c)) {
+          aux.add(c)
+        } else if (aux.count() > founds.count()) {
+          founds.clear()
+          founds.addAll(aux)
+          aux.clear()
+          array = array.drop(1).toCharArray()
+          break
+        } else {
+          aux.clear()
+          array = array.drop(1).toCharArray()
+          break
+        }
+      }
+    }
+
+    return founds.count().coerceAtLeast(1)
+  }
 }
